6ff9d977df5500ec25e09a7aff89d32fc78e7d82
Sava4 <sasha.savchenko@gmail.com>
2016-09-02 14:56:33 +0300

    Changed the HTML for the site

--- settings.py
+++ settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'djbl.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'djbl.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
